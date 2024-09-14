662623a7a9da34a1d5a7fe1bca44f5adf1bb3be2
Noid <nikitag.big2005@gmail.com>
2024-09-14 03:45:00 +0300

    add args and reading array from file

--- lab1/arrays_methods.rb
+++ lab1/arrays_methods.rb
@@ -13,11 +13,46 @@ end
 # Написать метод, который находит номер первого положительного элемента массива
 
 def first_positive_elem(arr)
-  for i in 0...arr.size
-    if arr[i] > 0
-      return i
-    end
-  end
-  return nil
+	for i in 0...arr.size
+		if arr[i] > 0
+		return i
+		end
+	end
+	return nil
 end
 
+
+def read_array(file)
+	array = []
+	begin
+		file_object = File.open(file, 'r')
+		while (line = file_object.gets)
+			elements = line.split
+			for elem in elements
+				array << elem.to_i
+			end
+		end
+		file_object.close
+    rescue 
+    puts "Файл не существует"
+    exit -1
+	end
+	return array
+end
+
+# Написать программу, которая принимает как аргумент два значения. Первое значение говорит, какой из методов задачи 5 выполнить, второй говорит о том, откуда читать список (аргументом должен быть написан адрес файла). Далее необходимо прочитать массив и выполнить метод.
+
+if ARGV.size == 2
+	method = ARGV[0].to_i
+	file = ARGV[1]
+    array = read_array(file)
+    if method == 1
+		puts "Минимальный элемент: #{min_elem(array)}"
+    elsif method == 2
+		puts "Номер первого положительного элемента: #{first_positive_elem(array)}"
+    else
+		puts "Введите номер 1 или 2"
+    end
+else
+	puts "Введите все аргументы программы: номер метода, который нужно выполнить и путь к файлу с массивом"
+end
