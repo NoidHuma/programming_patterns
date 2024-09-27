31d1993e2d1fef4c50fd08b0fc2aedb4b7405bf2
Noid <nikitag.big2005@gmail.com>
2024-09-28 02:37:49 +0300

    lab2 ex2 task4 - add student_short class

---
+++ students/student_short_class.rb
@@ -0,0 +1,29 @@
+load 'student_class.rb'
+
+class Student_short
+
+	attr_reader :id, :fullname, :contact, :git
+  
+	# конструктор, параметр либо объект экземпляр класса student, либо id и строка с краткой информацией
+	def initialize(student: nil, id: nil, student_info: nil)
+		if student
+			@id = student.id
+			parse_student_info(student.get_info)
+		elsif id && student_info
+			@id = id
+			parse_student_info(student_info)
+		end
+	end
+  
+	def parse_student_info(student_info)
+		info = student_info.split(", ")
+		@fullname = info[0]
+		@contact = info[1]
+		@git = info[2]
+	end
+	
+	def to_s
+		student_str = "ID: #{@id}\nFull name: #{@fullname}\nContact: #{@contact}\nGit: #{@git}\n"
+	end
+	
+end
