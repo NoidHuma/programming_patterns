020a677d57dcafce633d29c0e26df1d419e48dac
Noid <nikitag.big2005@gmail.com>
2024-10-15 20:58:53 +0300

    move validation to a separate file

--- students/student_class.rb
+++ students/student_class.rb
@@ -1,3 +1,5 @@
+load 'validator.rb'
+
 class Student
 
 	# аксессор
@@ -45,17 +47,12 @@ class Student
 		student_str = "ID: #{@id}\nName: #{@name}\nSurname: #{@surname}\nPatronymic: #{@patronymic}\nPhone: #{@phone}\nTelegram: #{@telegram}\nEmail: #{@email}\nGit: #{@git}\n"
 	end
 	
-	# Проверка корректности формата строки с фамилией/именем/отчеством
-	def self.valid_name?(name)
-		name =~ /^[а-яА-ЯёЁa-zA-Z]+$/
-	end
-	
 	# Сеттер для изменения фамилии
 	def surname=(surname)
 		if surname.nil? 
 			raise ArgumentError, "Фамилия - обязательное поле"
 		end
-		if self.class.valid_name?(surname)
+		if Validator.valid_name?(surname)
 			@surname = surname
 		else
 			raise ArgumentError, "Некорректный формат фамилии"
@@ -67,7 +64,7 @@ class Student
 		if name.nil? 
 			raise ArgumentError, "Имя - обязательное поле"
 		end
-		if self.class.valid_name?(name)
+		if Validator.valid_name?(name)
 			@name = name
 		else
 			raise ArgumentError, "Некорректный формат имени"
@@ -79,63 +76,43 @@ class Student
 		if patronymic.nil? 
 			raise ArgumentError, "Отчество - обязательное поле"
 		end
-		if self.class.valid_name?(patronymic)
+		if Validator.valid_name?(patronymic)
 			@patronymic = patronymic
 		else
 			raise ArgumentError, "Некорректный формат отчества"
 		end
 	end
 	
-	# Проверка корректности формата строки с телефоном
-	def self.valid_phone?(phone)
-		phone =~ /^(?:\+7|8)\d{10}$/
-	end
-	
 	# Сеттер для изменения телефона
 	private def phone=(phone)
-		if phone.nil? || self.class.valid_phone?(phone)
+		if phone.nil? || Validator.valid_phone?(phone)
 			@phone = phone
 		else
 			raise ArgumentError, "Некорректный формат телефонного номера"
 		end
 	end
 	
-	# Проверка корректности формата строки с телеграмом
-	def self.valid_telegram?(telegram)
-		telegram =~ /^(https:\/\/)?t\.me\/[a-zA-Z0-9_]{5,32}$/
-	end
-	
 	# Сеттер для изменения телеграма
 	private def telegram=(telegram)
-		if telegram.nil? || self.class.valid_telegram?(telegram)
+		if telegram.nil? || Validator.valid_telegram?(telegram)
 			@telegram = telegram
 		else
 			raise ArgumentError, "Некорректный формат ссылки на телеграм-аккаунт"
 		end
 	end
-  
-	# Проверка корректности формата строки с email
-	def self.valid_email?(email)
-		email =~ /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/
-	end
 	
 	# Сеттер для изменения email
 	private def email=(email)
-		if email.nil? || self.class.valid_email?(email)
+		if email.nil? || Validator.valid_email?(email)
 			@email = email
 		else
 			raise ArgumentError, "Некорректный формат email"
 		end
 	end
 	
-	# Проверка корректности формата строки с git
-	def self.valid_git?(git)
-		git =~ /^(https:\/\/)?github\.com\/[a-zA-Z0-9_-]{1,39}$/
-	end
-	
 	# Сеттер для изменения git
 	private def git=(git)
-		if git.nil? || self.class.valid_git?(git)
+		if git.nil? || Validator.valid_git?(git)
 			@git = git
 		else
 			raise ArgumentError, "Некорректный формат ссылки на github"
@@ -154,9 +131,9 @@ class Student
 	
 	# метод для изменения полей контактов
 	def set_contacts(phone: nil, telegram: nil, email: nil)
-		self.phone = phone if !phone.nil? 
-		self.telegram = telegram if !telegram.nil? 
-		self.email = email if !email.nil? 
+		self.phone = phone if !phone.nil?
+		self.telegram = telegram if !telegram.nil?
+		self.email = email if !email.nil?
 	end
 	
 	# получить фамилию и инициалы
---
+++ students/validator.rb
@@ -0,0 +1,41 @@
+class Validator
+
+	NAME_REGEX = /^[а-яА-ЯёЁa-zA-Z]+$/
+	FULLNAME_REGEX = /^[А-ЯЁA-Z][а-яёa-z-]*\s[А-ЯЁA-Z]\.[А-ЯЁA-Z]\.$/
+	PHONE_REGEX = /^(?:\+7|8)\d{10}$/
+	TELEGRAM_REGEX = /^(https:\/\/)?t\.me\/[a-zA-Z0-9_]{5,32}$/
+	EMAIL_REGEX = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/
+	GIT_REGEX = /^(https:\/\/)?github\.com\/[a-zA-Z0-9_-]{1,39}$/
+	
+
+	# Проверка корректности формата строки с фамилией/именем/отчеством
+	def self.valid_name?(name)
+		name =~ NAME_REGEX
+	end
+	
+	# Проверка корректности формата строки с телефоном	
+	def self.valid_phone?(phone)
+		phone =~ PHONE_REGEX
+	end
+	
+	# Проверка корректности формата строки с телеграмом
+	def self.valid_telegram?(telegram)
+		telegram =~ TELEGRAM_REGEX
+	end
+	
+	# Проверка корректности формата строки с email
+	def self.valid_email?(email)
+		email =~ EMAIL_REGEX
+	end
+		
+	# Проверка корректности формата строки с git
+	def self.valid_git?(git)
+		git =~ GIT_REGEX
+	end
+
+	# Проверка корректности формата строки с фамилией и инициалами
+	def self.valid_fullname?(fullname)
+		fullname =~ FULLNAME_REGEX
+	end
+	
+end
