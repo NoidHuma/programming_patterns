ebf64f3fb4561c80a4fc42189d4ae8a82b48655d
Noid <nikitag.big2005@gmail.com>
2024-10-19 01:50:13 +0300

    modify parse_student_info

--- students/student_short_class.rb
+++ students/student_short_class.rb
@@ -16,10 +16,19 @@ class StudentShort < StudentBase
 	end
   
 	private def parse_student_info(student_info)
-		info = student_info.split(", ")
-		@fullname = info[0]
-		@contact = info[1]
-		@git = info[2]
+		info = student_info.split(", ").map do |part|
+			piece = part.split("=")
+			case piece[0].strip
+			when "Fullname"
+				@fullname = piece[1].strip
+			when "Contact"
+				@contact = piece[1].strip
+			when "Git"
+				@git = piece[1].strip
+			else
+				raise ArgumentError, "Неверный формат строки"
+			end
+		end
 	end
 	
 	def get_info()
