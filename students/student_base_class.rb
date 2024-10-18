1cfefa69d5ddd9d9538bbaaf4a7cb65fff8a7f80
Noid <nikitag.big2005@gmail.com>
2024-10-19 01:25:28 +0300

    modify StudentBase

--- students/student_base_class.rb
+++ students/student_base_class.rb
@@ -3,6 +3,8 @@ require_relative 'validator.rb'
 class StudentBase
 
 	attr_reader :id,:git
+	
+	protected
 
 	def initialize(id: nil, git: nil)
 		@id = id
@@ -14,11 +16,19 @@ class StudentBase
 	end
 
     def has_git?()
-		return !@git.nil?    
+		return !@git.nil?
 	end
 
     def has_contact?()
 		return !(@phone.nil? && @telegram.nil? && @email.nil? && @contact.nil?)
     end
 	
+	def get_info
+		raise NotImplementedError, "Метод get_info должен быть реализован"
+	end
+  
+	def to_s
+		raise NotImplementedError, "Метод to_s должен быть реализован"
+	end
+  
 end
