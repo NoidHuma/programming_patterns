7e081a7de99e9533b7479cc310a37b4a852a4738
Noid <nikitag.big2005@gmail.com>
2025-01-15 13:56:57 +0300

    lab4 3(2) refactor insert script

--- students/database/insert_scripts/insert.sql
+++ students/database/insert_scripts/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO student (name, surname, patronymic, phone, email, telegram, git)
 VALUES
-    ('Ivan', 'Ivanov', 'Ivanovich', '123-456-7890', 'ivan@example.com', 'ivan_telegram', 'ivan_github'),
-    ('Maria', 'Petrova', 'Petrovna', '234-567-8901', 'maria@example.com', 'maria_telegram', 'maria_github'),
-    ('Petr', 'Sidorov', 'Petrovich', '345-678-9012', 'petr@example.com', 'petr_telegram', 'petr_github'),
-    ('Anna', 'Orlova', 'Andreevna', '456-789-0123', 'anna@example.com', 'anna_telegram', 'anna_github'),
-    ('Olga', 'Mikhailova', 'Mikhailovna', '567-890-1234', 'olga@example.com', 'olga_telegram', 'olga_github'),
-    ('Alexey', 'Kuznetsov', 'Alexandrovich', '678-901-2345', 'alexey@example.com', 'alexey_telegram', 'alexey_github'),
-    ('Dmitry', 'Smirnov', 'Dmitrievich', '789-012-3456', 'dmitry@example.com', 'dmitry_telegram', 'dmitry_github'),
-    ('Elena', 'Volkova', 'Viktorovna', '890-123-4567', 'elena@example.com', 'elena_telegram', 'elena_github'),
-    ('Andrey', 'Kozlov', 'Andreevich', '901-234-5678', 'andrey@example.com', 'andrey_telegram', 'andrey_github');
+    ('Ivan', 'Ivanov', 'Ivanovich', '+71234567890', 'ivan@example.com', 'https://t.me/ivan_telegram', 'https://github.com/ivan_github'),
+    ('Maria', 'Petrova', 'Petrovna', '+72345678901', 'maria@example.com', 'https://t.me/maria_telegram', 'https://github.com/maria_github'),
+    ('Petr', 'Sidorov', 'Petrovich', '+73456789012', 'petr@example.com', 'https://t.me/petr_telegram', 'https://github.com/petr_github'),
+    ('Anna', 'Orlova', 'Andreevna', '+74567890123', 'anna@example.com', 'https://t.me/anna_telegram', 'https://github.com/anna_github'),
+    ('Olga', 'Mikhailova', 'Mikhailovna', '+75678901234', 'olga@example.com', 'https://t.me/olga_telegram', 'https://github.com/olga_github'),
+    ('Alexey', 'Kuznetsov', 'Alexandrovich', '+76789012345', 'alexey@example.com', 'https://t.me/alexey_telegram', 'https://github.com/alexey_github'),
+    ('Dmitry', 'Smirnov', 'Dmitrievich', '+77890123456', 'dmitry@example.com', 'https://t.me/dmitry_telegram', 'https://github.com/dmitry_github'),
+    ('Elena', 'Volkova', 'Viktorovna', '+78901234567', 'elena@example.com', 'https://t.me/elena_telegram', 'https://github.com/elena_github'),
+    ('Andrey', 'Kozlov', 'Andreevich', '+79012345678', 'andrey@example.com', 'https://t.me/andrey_telegram', 'https://github.com/andrey_github');
